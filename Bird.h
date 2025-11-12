2efef0ff7e5e84a06e4bda1c79049f7c7670feb9
nourhan-zahran <nourhanhassanzahran@gmail.com>
2025-11-12 15:21:58 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -4,7 +4,20 @@
 #include "Animal.h"
 
 //define class bird here 
+class Bird : public Animal
+{
+private:
+    float wingSpan;
 
+public:
+    Bird();
+    Bird(string name,int age,  bool isHungry, float wingSpan);
+
+    virtual ~Bird();
+
+    void setWingSpan(float wingSpan);
+    float getWingSpan();
+};
 
 
 
