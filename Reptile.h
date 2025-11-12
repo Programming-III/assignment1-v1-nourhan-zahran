470e6aef2d3bacdc0312433aee4493741f7d97e4
nourhan-zahran <nourhanhassanzahran@gmail.com>
2025-11-12 15:23:11 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -4,7 +4,20 @@
 #include "Animal.h"
 
 //define reptile class here
+class Reptile : public Animal
+{
+private:
+    bool isVenomous;
 
+public:
+    Reptile();
+    Reptile(string name,int age,  bool isHungry, bool isVenomous);
+
+    virtual ~Reptile();
+
+    void setVenomous(bool isVenomous);
+    bool getVenomous();
+};
 
 
 
