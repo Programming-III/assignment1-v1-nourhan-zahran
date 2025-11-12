3d55e00175c52da4d441310079d357b827ba53eb
nourhan-zahran <nourhanhassanzahran@gmail.com>
2025-11-12 15:20:53 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -6,6 +6,28 @@ using namespace std;
 
 //define animal class here 
 
+class Animal
+{
+private:
+    string name;
+    int age;
+    bool isHungry;
 
+public:
+    Animal();
+    Animal(string name,int age,  bool isHungry );
+
+    virtual ~Animal();
+
+    void setName(string name);
+    string getName();
+    void setAge (int age);
+    int getAge();
+    void setHungry (bool isHungry);
+    bool getHungry();
+
+   virtual void display();
+   virtual void feed();
+};
 
 #endif
