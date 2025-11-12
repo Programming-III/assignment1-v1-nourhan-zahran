c375840b45c1697c0ccdace276e8d640c584c447
nourhan-zahran <nourhanhassanzahran@gmail.com>
2025-11-12 15:22:45 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -4,7 +4,21 @@
 #include "Animal.h"
 
 //define mammal class here
+class Mammal : public Animal
+{
+private:
+    string furColor;
 
+public:
+    Mammal();
+    Mammal(string name,int age,  bool isHungry, string furColor);
+
+    virtual ~Mammal();
+
+    void setFurColor(string furColor);
+    string getFurColor();
+
+};
 
 
 
